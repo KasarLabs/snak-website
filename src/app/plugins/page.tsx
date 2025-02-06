fc06c05e6d2c2756fe5c05ff7a8cec9f3198a84e
minhanhld <minhanh_leduc@outlook.fr>
2025-02-06 18:03:26 +0100

    ran npx prettier

--- src/app/plugins/page.tsx
+++ src/app/plugins/page.tsx
@@ -268,7 +268,7 @@ export default function HomePage() {
         <PluginModal
           plugin={selectedCircle}
           isVisible={isModalVisible}
-		  expandedActions={expandedActions}
+          expandedActions={expandedActions}
           onClose={() => {
             setIsModalVisible(false);
             setExpandedActions(new Set());
